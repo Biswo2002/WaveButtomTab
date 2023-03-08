fb7ac8955304008f79ebc4c1112e2ab7c58b73b6
Biswopaban Nayak <biswopabannayak@gmail.com>
2023-03-08 12:21:46 +0530

    first COmmit

---
+++ src/routes/Bottomtab.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Bottomtab = () => {
+  return (
+    <View>
+      <Text>Bottomtab</Text>
+    </View>
+  )
+}
+
+export default Bottomtab
+
+const styles = StyleSheet.create({})
---
+++ src/screen/Wave.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Wave = () => {
+  return (
+    <View>
+      <Text>Wave</Text>
+    </View>
+  )
+}
+
+export default Wave
+
+const styles = StyleSheet.create({})
